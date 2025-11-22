89cca7c3f9bab767b0a215abecaf9fd7e9cc9185
vniekers <tiny@tinyandjudycom>
2025-11-22 11:59:23 -0500

    Remove legacy Auth0 route and old backup page

--- frontend/app/api/auth/[...auth0]/route.ts
+++
@@ -1,7 +0,0 @@
-import { handleAuth } from "@auth0/nextjs-auth0/edge";
-//import { handleAuth } from "@auth0/nextjs-auth0";
-// Not needed with this version:
-export const runtime = "edge";
-
-export const GET = handleAuth();
-export const POST = handleAuth();
